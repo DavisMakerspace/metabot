dc19477487b6470352be9689ef5b6fe099735b63
Braden Pellett <git@dabrado.net>
2014-07-26 22:07:37 -0700

    Pass string conversion as a Proc

--- lib/receiver.rb
+++ lib/receiver.rb
@@ -1,13 +1,13 @@
 require 'logger'
 module Receiver
   class DebugLog
-    def initialize(log=Logger.new(STDERR), level:Logger::DEBUG, progname:nil, to_s:(:to_s))
+    def initialize(log=Logger.new(STDERR), level:Logger::DEBUG, progname:nil, to_s:->(msg){msg.to_s})
       @log = log
       @level = level
       @progname = progname
       @to_s = to_s
     end
-    def call(msg); @log.add(@level,nil,@progname){msg.send(@to_s)}; end
+    def call(msg); @log.add(@level,nil,@progname){@to_s.call(msg)}; end
   end
   class Multi
     def initialize
